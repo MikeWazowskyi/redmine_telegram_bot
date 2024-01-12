6650dad30aa2525b3924f87932136438dfc62078
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2024-01-12 14:52:38 +0400

    feature: timeout script added.

--- redmine_telegram_bot/server-entrypoint.sh
+++ redmine_telegram_bot/server-entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-until cd /app/; do
-  echo "Waiting for server volume..."
+TIMEOUT=30
+start_time=$(date +%s)
+
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+
+    if [ $elapsed_time -ge $TIMEOUT ]; then
+        echo "Ошибка: Превышено время ожидания. Не удалось установить соединение в течение $TIMEOUT секунд."
+        exit 1
+    fi
+
+    echo "Ожидание подключения к PostgreSQL. Прошло времени: $elapsed_time секунд."
+    sleep 0.1
 done
+
+echo "Соединение успешно установлено с PostgreSQL."
 
 # Apply database migrations
 echo "Applying database migrations ..."
@@ -23,4 +37,4 @@ python manage.py initadmin
 
 # Start server
 echo "Starting server ..."
-gunicorn redmine_telegram_bot.wsgi:application --bind 0.0.0.0:8000  #python manage.py runpolling
+gunicorn redmine_telegram_bot.wsgi:application --bind 0.0.0.0:8000 & python manage.py runpolling
